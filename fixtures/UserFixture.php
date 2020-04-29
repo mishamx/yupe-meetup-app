a4c75b65b7ba35b44fe69945ba8f5e037c663d75
Anton Sitnikov <koma136@mail.ru>
2020-04-29 22:28:12 +0300

    user fixture and test

---
+++ fixtures/UserFixture.php
@@ -0,0 +1,13 @@
+<?php
+namespace fixtures;
+
+use yii\test\ActiveFixture;
+
+/**
+ * Class UserFixture
+ * @package common\fixtures
+ */
+class UserFixture extends ActiveFixture
+{
+    public $modelClass = 'models\User';
+}
